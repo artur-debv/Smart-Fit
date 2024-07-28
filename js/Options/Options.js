169aaa6a55b52802b287e7961953df0abfe58ae9
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-07-28 17:08:41 -0300

    updates

--- js/Options/Options.js
+++ js/Options/Options.js
@@ -2,6 +2,12 @@ const Option = document.querySelectorAll('input[type="radio"]');
 
 const clean = document.querySelector('.clean');
 
+const Closed = document.querySelector('.containers');
+
+closed.addEventListener('change', (event) => {
+    event.checked = false
+})
+
 clean.addEventListener('click', (event) => {
     event.preventDefault();
     Option.forEach((e) => {
