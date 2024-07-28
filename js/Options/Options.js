1e735b20ce5063dc7c98c72630aa17d9295d3296
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-07-28 16:45:53 -0300

    updates

--- js/Options/Options.js
+++ js/Options/Options.js
@@ -2,6 +2,6 @@ const Option = document.querySelectorAll('input[name="hours"]');
 
 Option.forEach((e) => {
     e.addEventListener('change', (event) => {
-        console.log(`Você selecionou: ${event.target.value}`)
+        console.log(`Você selecionou: ${event.target.name}`)
     })
 })
