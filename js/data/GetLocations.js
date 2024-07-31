28183ebb922187239bb3a84bbc4019e3bacf3053
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-07-30 21:55:11 -0300

    refactor: I changed the content value of the results property

--- js/data/GetLocations.js
+++ js/data/GetLocations.js
@@ -11,7 +11,21 @@ searchgyms.addEventListener('click', (event) => {
         .then(data => { 
           const result = document.querySelector('.result')
           result.innerText = `Resultados encontrados: ${data.locations.length}`
+
+          data.locations.forEach(element => {
+            const box = document.createElement('div')
+            box.classList.add('box')
+            box.innerHTML = `
+                <h3>${element.content}</h3>
+                <p>${element.mask}</p>
+                <p>${element.towel}</p>
+                <p>${element.fountain}</p>
+                <p>${element.lockerroom}</p>
+            `
+
         });
 })
+})
+
 
 
