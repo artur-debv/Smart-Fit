c19bc1a9a232401e008135ef26338b983e96d2cf
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-01 11:23:36 -0300

    updates

--- js/data/GetLocations.js
+++ js/data/GetLocations.js
@@ -16,7 +16,7 @@ searchgyms.addEventListener('click', (event) => {
 
 
             if (closed.checked) {
-                data.locations = data.locations.filter(element => element.opened = false);
+                data.locations = data.locations.filter(element => element.opened === false);
             }
 
 
