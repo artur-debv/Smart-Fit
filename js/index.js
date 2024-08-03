78b09142aacd2ec2fbcdc37c3844f96950082ee7
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-03 15:45:41 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -24,3 +24,39 @@ const OpenAcademys = async () => {
     DiplayCount(academysFilter)
 }
 document.addEventListener("DOMContentLoaded", OpenAcademys)
+
+
+function academysCards(academys){
+    const cards = document.querySelector(".cards")
+    academys.forEach(academys => {
+        const card = document.createElement("div")
+        card.classList.add("card")
+        card.innerHTML = `
+        <div class="card__image">
+`
+
+        cards.appendChild(card)
+})
+}
+
+const Searchs = async () => {
+    const academys = await FetchApi();
+    const academysFilter = filterAcademy(academys)
+
+    const Closed = document.getElementById("#closed")
+
+    if(Closed.checked){
+        DiplayCount(academys)
+        academysCards(academys)
+    }else{
+        DiplayCount(academysFilter)
+        academysCards(academysFilter)
+    }
+
+}
+
+const SearchAcademys = async () => {
+    Search()
+}
+
+Search.addEventListener("click", SearchAcademys)
