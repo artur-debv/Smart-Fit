073e68c3c9a5c368d0b55d1abacbba08490434ca
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-04 18:23:25 -0300

    feat: I added the unit search functionality, I also created a validation

--- js/searchGyms/SearchGyms.js
+++ js/searchGyms/SearchGyms.js
@@ -1,5 +1,6 @@
 const searchButtons = document.querySelector(".search_button");
 const searchInput = document.querySelector(".search_input");
+const Status = document.querySelector(".status");
 
 searchButtons.addEventListener("click", async (event) => {
     event.preventDefault();
@@ -11,4 +12,18 @@ searchButtons.addEventListener("click", async (event) => {
 
     displayCount(filteredAcademies);
     academiesCards(filteredAcademies);
+
+    if (filteredAcademies.length === 0) {
+        Status.innerHTML = `
+            <div class="uk-alert uk-alert-danger" uk-alert>
+                <a class="uk-alert-close" uk-close></a>
+                <div class="uk-alert-description">
+                    Nenhum resultado encontrado. Tente novamente com outro nome.
+                </div>
+            </div>
+        `;
+    } else {
+        Status.textContent = "";
+    }
+
 });
