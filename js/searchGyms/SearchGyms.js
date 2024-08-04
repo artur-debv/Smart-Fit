629d8116de7fcd86b887842ba616c24211d2d886
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-04 16:23:03 -0300

    updates

---
+++ js/searchGyms/SearchGyms.js
@@ -0,0 +1,6 @@
+const Button_Search = document.querySelector(".search_button");
+
+Button_Search.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("Hello World!");
+})
