625799f81c91eeacf02bb9564480ed97b215c202
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-04 18:51:16 -0300

    updates

--- js/searchGyms/SearchGyms.js
+++ js/searchGyms/SearchGyms.js
@@ -2,9 +2,6 @@ const searchButtons = document.querySelector(".search_button");
 const searchInput = document.querySelector(".search_input");
 const Status = document.querySelector(".status");
 
-import { uikit } from "https://cdn.jsdelivr.net/npm/uikit@3.15.20/dist/js/uikit.min.js";
-import { uikitIcons } from "https://cdn.jsdelivr.net/npm/uikit@3.15.20/dist/js/uikit-icons.min.js";
-
 searchButtons.addEventListener("click", async (event) => {
     event.preventDefault();
 
