8974f908ea96973a9a178b790c04992a269cb394
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-04 18:06:13 -0300

    feat: I added the functionality to search by units

--- js/searchGyms/SearchGyms.js
+++ js/searchGyms/SearchGyms.js
@@ -1,14 +1,24 @@
-const searchButtons = document.querySelector(".search_button");
+const searchButton = document.querySelector(".search_button");
 const searchInput = document.querySelector(".search_input");
 
-searchButtons.addEventListener("click", async (event) => {
+searchButton.addEventListener("click", async (event) => {
     event.preventDefault();
+    searchAcademies();
+});
 
+searchInput.addEventListener("input", async () => {
+    searchAcademies();
+});
+
+const searchAcademies = async () => {
     const searchValue = searchInput.value.toLowerCase();
     const academies = await fetchApi();
     const academiesFilter = filterAcademy(academies);
-    const filteredAcademies = academiesFilter.filter(academy => academy.title.toLowerCase().includes(searchValue));
+
+    const filteredAcademies = searchValue
+        ? academiesFilter.filter(academy => academy.title.toLowerCase().includes(searchValue))
+        : academiesFilter;
 
     displayCount(filteredAcademies);
     academiesCards(filteredAcademies);
-});
+};
