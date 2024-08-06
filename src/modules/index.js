452d4b1ba909fd16b1585d09e45b6a35f58521cc
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-05 22:45:10 -0300

    refactor: d

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,31 +1,31 @@
 import { fetchApi } from './modules/fetchApi.js';
-import { filterAcademy } from './modules/filterAcademy.js';
-import { displayCount } from './modules/displayCount.js';
-import { academiesCards } from './modules/academiesCards.js';
-import { searchAcademies } from './modules/searchAcademies.js';
+import { filterAcademy } from '.filterAcademy.js';
+import { displayCount } from '.displayCount.js';
+import { academiesCards } from '.academiesCards.js';
+import { SearchGyms } from './SearchGyms.js';
 
 const searchButton = document.querySelector(".search_button");
 const searchInput = document.querySelector(".search_input");
 const Status = document.querySelector(".status");
 
 const openAcademies = async () => {
-    const academies = await fetchApi();
-    const academiesFilter = filterAcademy(academies);
-    displayCount(academiesFilter);
-    academiesCards(academiesFilter); // Adiciona esta linha para exibir as academias abertas ao carregar a página
+  const academies = await fetchApi();
+  const academiesFilter = filterAcademy(academies);
+  displayCount(academiesFilter);
+  academiesCards(academiesFilter); // Adiciona esta linha para exibir as academias abertas ao carregar a página
 };
 
 document.addEventListener("DOMContentLoaded", openAcademies);
 
 searchButton.addEventListener("click", async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const searchValue = searchInput.value.toLowerCase();
-    const statusMessage = await searchAcademies(searchValue);
+  const searchValue = searchInput.value.toLowerCase();
+  const statusMessage = await SearchGyms(searchValue);
 
-    if (statusMessage) {
-        Status.textContent = alert(statusMessage);
-    } else {
-        Status.textContent = "";
-    }
+  if (statusMessage) {
+    Status.textContent = alert(statusMessage);
+  } else {
+    Status.textContent = "";
+  }
 });
