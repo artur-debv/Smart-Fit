235bfaa2844f9ccb98bd4d8baf2966082c006cb0
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-06 22:40:54 -0300

    updates

--- src/modules/SearchGyms.js
+++ src/modules/SearchGyms.js
@@ -1,13 +1,14 @@
 import { fetchApi } from './fetchApi.js';
 import { filterAcademy } from './filterAcademy.js';
 import { academiesCards } from './academiesCards.js';
+import { count } from './count.js';
 
 export const SearchGymss = async (searchValue) => {
   const academies = await fetchApi();
   const academiesFilter = filterAcademy(academies);
   const filteredAcademies = academiesFilter.filter(academy => academy.title.toLowerCase().includes(searchValue));
 
-  displayCount(filteredAcademies);
+  count(filteredAcademies);
   academiesCards(filteredAcademies);
 
   return filteredAcademies.length === 0 ? "Nenhum resultado encontrado" : "";
